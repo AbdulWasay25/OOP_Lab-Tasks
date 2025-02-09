2d9d0e54b77afc1dd7afbd5ab3289a1d2230e7fa
AbdulWasay25 <awasay2500@gmail.com>
2025-02-09 16:27:52 +0500

    Update Task_3.cpp

--- Lab_2/Task_3.cpp
+++ Lab_2/Task_3.cpp
@@ -1,17 +1,26 @@
 #include<iostream>
-#include<cstdlib>
-
-using namespace std ;
- 
-int main(int argc , char* argv[]){
-	int n=5 ;
-	int *array[n] ;
-	if(argc>n){
-		*array = new int[2 * n] ;
-	}
-	//*array = new int[argc];
-	cout<<"Program For Memory Allocation Successfull" ; 
-	delete [] array;
-	
-
+using namespace std;
+int main(int argc , char* argv[]) {
+    int size = 5;
+    int* arr = new int[size];  
+    int count = 0;
+    for (int i = 0 , k=1 ; count < argc-1; i++ , k++) {
+        arr[count] = stoi(*(argv + k));  
+        count++;
+        if (count == size) {
+            size *= 2;
+            int* temp = new int[size];  
+            for (int j = 0; j < count; j++) {
+                temp[j] = arr[j]; 
+                cout<<" "<<temp[j];
+            }
+            cout<<endl;
+            delete[] arr;  
+            arr = temp;  
+        }
+    }
+	cout << "Program For Memory Allocation Succesfull"  << endl;
+    cout << "Array size: " << size << endl;
+    delete[] arr;  
+    return 0;
 }
