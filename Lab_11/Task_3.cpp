31e515afad69d37fcb47ae7ca22d4c822b31e5bb
AbdulWasay25 <awasay2500@gmail.com>
2025-05-07 21:41:47 +0500

    Add files via upload

---
+++ Lab_11/Task_3.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <exception>
+using namespace std;
+
+class ArrayIndexOutOfBounds : public exception {
+public:
+    const char* what() const noexcept override {
+        return "Array index out of bounds.";
+    }
+};
+
+template<typename T>
+class SafeArray {
+    T* arr;
+    int size;
+public:
+    SafeArray(int sz) : size(sz) {
+        arr = new T[size];
+    }
+
+    ~SafeArray() {
+        delete[] arr;
+    }
+
+    T& operator[](int index) {
+        if (index < 0 || index >= size)
+            throw ArrayIndexOutOfBounds();
+        return arr[index];
+    }
+
+    int getSize() const { return size; }
+};
+
+
+int main() {
+    try {
+        SafeArray<int> arr(5);
+        for (int i = 0; i < arr.getSize(); i++)
+            arr[i] = i * 10;
+
+        for (int i = 0; i < arr.getSize(); i++)
+            cout << arr[i] << " ";
+        cout << endl;
+
+        // cout << arr[10];     // Uncomment for exception
+
+    } catch (exception& e) {
+        cout << "Exception: " << e.what() << endl;
+    }
+    return 0;
+}
